4c31371da4f59d733e4fa16961b1200394e28e37
Tonya Mork <hellofromtonya@knowthecode.io>
2020-01-31 16:40:48 -0600

    Simplified autoPurge test by removing unnecessary code.

--- Tests/Integration/CloudflareSubscriber/autoPurge.php
+++ Tests/Integration/CloudflareSubscriber/autoPurge.php
@@ -33,10 +33,6 @@ class Test_AutoPurge extends TestCase {
 	}
 
 	public function testShouldBailoutWhenUserCantPurgeCF() {
-		$data = [ 'do_cloudflare' => 1 ];
-		update_option( 'wp_rocket_settings', $data );
-		self::$options->set_values( $data );
-
 		$user = $this->factory->user->create( [ 'role' => 'contributor' ] );
 		wp_set_current_user( $user );
 
@@ -46,17 +42,6 @@ class Test_AutoPurge extends TestCase {
 	}
 
 	public function testShouldBailoutWhenNoPageRule() {
-		$data = [
-			'cloudflare_email'   => self::$email,
-			'cloudflare_api_key' => self::$api_key,
-			'cloudflare_zone_id' => self::$zone_id,
-			'do_cloudflare'      => 1,
-		];
-		update_option( 'wp_rocket_settings', $data );
-		self::$options->set_values( $data );
-
-		add_filter( 'site_url', [ $this, 'setSiteUrl' ] );
-
 		$admin = get_role( 'administrator' );
 		$admin->add_cap( 'rocket_purge_cloudflare_cache' );
 		$user = $this->factory->user->create( [ 'role' => 'administrator' ] );
@@ -71,11 +56,5 @@ class Test_AutoPurge extends TestCase {
 			->with( 0 );
 
 		do_action( 'after_rocket_clean_domain' );
-
-		remove_filter( 'site_url', [ $this, 'setSiteUrl' ] );
-	}
-
-	public function setSiteUrl() {
-		return self::$site_url;
 	}
 }
